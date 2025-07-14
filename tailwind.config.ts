5fa0e38060cdcb9062694519ed0083a376c1f721
Ahmed Osman <Ahmedot2@gmail.com>
2025-07-14 04:52:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <p class="mx-auto

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ export default {
     },
     extend: {
       fontFamily: {
-        body: ['"Inter"', 'sans-serif'],
+        body: ['"DM Sans"', 'sans-serif'],
         headline: ['"Michroma"', 'sans-serif'],
       },
       colors: {
