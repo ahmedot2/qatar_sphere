4247a0b4c82e5e0f56ce6f1a8b55088e1ea05e41
Ahmed Osman <Ahmedot2@gmail.com>
2025-07-16 19:47:16 +0000

    Change the color of the hero section title only to maroon red like Qatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -137,7 +137,7 @@ export default function Home({}) {
           className="relative flex h-[calc(100vh-4rem)] flex-col items-center justify-center text-center p-4"
         >
           <GlassCard className="w-full">
-            <h1 className="font-display text-4xl font-bold tracking-wider md:text-6xl lg:text-7xl uppercase">
+            <h1 className="font-display text-4xl font-bold tracking-wider md:text-6xl lg:text-7xl uppercase text-red-800">
               QatarSphere District
             </h1>
             <DecryptedText
